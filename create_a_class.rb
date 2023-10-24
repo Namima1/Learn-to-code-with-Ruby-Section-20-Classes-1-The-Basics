e820084d3936b625d0182cec1d9030a82bc93ca3
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-10-24 11:54:11 -0400

    Add files via upload

---
+++ create_a_class.rb
@@ -0,0 +1,20 @@
+#UpperCamelCase - Capitalize first letter of every word 
+
+class Guitar 
+  # Logic belonging to the class 
+end 
+
+acoustic = Guitar.new
+electric = Guitar.new 
+hybrid = Guitar.new
+
+puts acoustic 
+puts electric 
+puts hybrid
+
+#this will give a default output 
+#the output will be the class the object is made of 
+#it is the default object position 
+
+
+ 
